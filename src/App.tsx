e4d7dd6e44709f247d881bad101b34af47897278
Jirka Ketner <jirka@meiro.io>
2021-02-24 22:26:29 +0100

    boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Web3Provider } from "@ethersproject/providers"
+import { Web3ReactProvider } from "@web3-react/core"
+import Wallet from "utils/Wallet"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getLibrary = (provider: any): Web3Provider => {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
 }
 
-export default App;
+const App = () => {
+  const a = "tada"
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Wallet />
+    </Web3ReactProvider>
+  )
+}
+
+export default App
