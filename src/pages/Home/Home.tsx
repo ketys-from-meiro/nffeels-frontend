d0cedc419851702d15112399acbb235687ac7265
Lewis Freiberg <freiberg.lewis@gmail.com>
2021-03-31 17:25:58 +0200

    Add button toggle for modal

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,15 +4,25 @@ import styles from "./Home.module.scss"
 import wojakImgSrc from "../../images/wojak-landing.png"
 import Button from "components/Button/Button"
 import LoginModalContext from "../../App/components/LoginModal/context"
+import { useWeb3 } from "../../contexts/useWeb3"
 
 const Home = () => {
+  const { account } = useWeb3()
   const { toggle } = useContext(LoginModalContext)
+
   return (
     <section className={styles.homePage}>
       <img src={wojakImgSrc} alt="Wojak" className={styles.wojakImg} />
-      <Button className={styles.lotteryButton} color="secondary" size="lg">
-        Enter lottery
-      </Button>
+
+      {account ? (
+        <Button className={styles.lotteryButton} color="secondary" size="lg">
+          Enter lottery
+        </Button>
+      ) : (
+        <Button className={styles.lotteryButton} color="secondary" size="lg" onClick={toggle}>
+          Connect Wallet
+        </Button>
+      )}
     </section>
   )
 }
