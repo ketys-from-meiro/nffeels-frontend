2501b6f6f266d593869297594c36a20ccc18f6e4
Jirka Ketner <jirka@meiro.io>
2021-04-23 23:21:56 +0200

    Minor gallery improvements, usage of Button component,...

--- src/pages/Gallery/Gallery.tsx
+++ src/pages/Gallery/Gallery.tsx
@@ -4,12 +4,15 @@ import styles from "./Gallery.module.scss"
 import Lines from "components/Lines/Lines"
 import Dropdown from "components/Dropdown/Dropdown"
 import Slideover from "components/Slideover/Slideover"
-import classnames from "classnames"
+import Button from "components/Button/Button"
+
+const DROPDOWN_OPTIONS = ["Trait 1", "Trait 2"]
 
 const Gallery = () => {
   const [isButtonShown, setIsButtonShown] = useState({ key: -1, state: false })
   const [isSideoverShown, setIsSideoverShown] = useState(false)
   const [currentWojakId, setCurrentWojakId] = useState(0)
+  const [filterOption, setFilterOption] = useState("")
 
   return (
     <>
@@ -22,18 +25,15 @@ const Gallery = () => {
         <div className={styles.galleryContainer}>
           <div className={styles.headerContainer}>
             <div>
-              <Dropdown />
-              <div className={styles.counterContainer}>
-                <div>
-                  <button
-                    type="button"
-                    className={classnames(styles.dropdownButton, styles.countLabel)}
-                    id="options-menu"
-                  >
-                    My Wojaks: 0
-                  </button>
-                </div>
-              </div>
+              <Dropdown
+                options={DROPDOWN_OPTIONS}
+                value={filterOption}
+                onChange={option => {
+                  console.log("setting filter: ", option)
+                  setFilterOption(option)
+                }}
+              />
+              <div className={styles.myWojaksCounter}>My Wojaks: 0</div>
             </div>
 
             <h2 className={styles.pageTitle}>Wojak Gallery</h2>
@@ -50,26 +50,29 @@ const Gallery = () => {
                   <div className={styles.linesContainer}>
                     <Lines />
                   </div>
-                  <img
+                  <div
+                    className={styles.itemPicker}
                     onClick={() => {
                       setIsSideoverShown(true)
                       setCurrentWojakId(index)
                     }}
-                    className={styles.imageStyles}
-                    src="https://cdn.discordapp.com/attachments/812822477683818527/825861306283589652/native-human.png"
-                    alt=""
-                  />
+                  >
+                    <img
+                      className={styles.imageStyles}
+                      src="https://cdn.discordapp.com/attachments/812822477683818527/825861306283589652/native-human.png"
+                      alt=""
+                    />
+                  </div>
+
                   <div className={styles.legendContainer}>
                     <div className={styles.imageTitleContainer}>
                       <h3 className={styles.imageTitle}>#{index}</h3>
                     </div>
-                    <div className={styles.viewButtonContainer}>
-                      {isButtonShown.key === index && isButtonShown.state && (
-                        <button type="button" className={styles.viewButton}>
-                          View on Opensea
-                        </button>
-                      )}
-                    </div>
+                    {isButtonShown.key === index && isButtonShown.state && (
+                      <Button color="grey" type="button" className={styles.viewButton}>
+                        View on Opensea
+                      </Button>
+                    )}
                   </div>
                 </div>
               </li>
